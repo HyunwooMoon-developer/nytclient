a173e43c1a48e9a6657c4f6aeeaff515108f1b2b
Hyunwoo Moon <mhw9163@gmail.com>
2020-12-29 10:29:50 -0800

    practice

---
+++ node-express-book/app.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+const express= require('express');
+const morgan = require('morgan');
+const books = require('./book');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('common'));
+
+app.get('/books', (req, res) =>{
+    const {search= "", sort} = req.query;
+    if(sort){
+        if(!['title', 'rank'].includes(sort)){
+            return res.status(400).send('sort must be one of title or rank');
+        }
+    }
+
+    let results = books
+                .filter(book =>
+                    book.title
+                    .toLowerCase()
+                    .includes(search.toLowerCase()));
+    if(sort){
+        results
+        .sort((currentBook, nextBook)=>{
+            return currentBook[sort] > nextBook[sort] ? 1:
+            currentBook[sort] < nextBook[sort] ? -1 : 0;
+        })
+    }
+
+   
+res.json(results);
+})
+
+app.listen(8000, () => {
+    console.log('localhost 8000 connected');
+})
---
+++ src/Book.js
@@ -0,0 +1,21 @@
+import moment from 'moment';
+import React from 'react';
+
+const Book = (props) => {
+    return (
+        <div className="book">
+            <h2>{props.title}</h2>
+            <div className="book_author">by {props.author}</div>
+            <div className="book_publisher">
+                Published by : {props.publisher}
+                on {moment(props.published_date).format('DD MMM YYYY')}
+            </div>
+            <div className="book_description">{props.description}</div>
+            <div className="book_details">
+                Rank {props.rank} this week
+            </div>
+        </div>
+    );
+};
+
+export default Book;
